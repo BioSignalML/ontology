0d506d0007920328578fbfea87fb7bc0d98b4145
Dave Brooks <dave@bcs.co.nz>
2012-01-29 10:20:42 +1300

    Get description from dc:description (as well as rdfs:comment).

--- generate.py
+++ generate.py
@@ -8,6 +8,7 @@ SOURCE = '2011-04-biosignalml.ttl'
 
 
 PREFIXES = { 'owl':  'http://www.w3.org/2002/07/owl#',
+             'dc':   'http://purl.org/dc/elements/1.1/',
              'rdfs': 'http://www.w3.org/2000/01/rdf-schema#',
              'bsml':  BSML_NAMESPACE,
            }
@@ -47,17 +48,19 @@ if __name__ == '__main__':
   g = rdf.Graph.create_from_resource(fullname, rdf.Format.TURTLE)
   for r in g.query('\n'.join(['prefix %s: <%s>' % (p, u) for p, u in PREFIXES.iteritems()]
                            + ['',
-                              'select ?class ?subject ?comment where {',
+                              'select ?class ?subject ?desc ?comment where {',
                               '  ?subject a ?class .',
+                              '  optional { ?subject dc:description ?desc } .',
                               '  optional { ?subject rdfs:comment ?comment } .',
                               '  filter( ' + ' || '.join(['?class = %s' % c for c in CLASSES]) + ' )',
                               '  } order by ?class ?subject'])):
     cls  = abbreviate(r['class'])
     subj = abbreviate(r['subject'])
-    doc  = abbreviate(r['comment'])   ## Why abbreviate ???
+    doc = [ ]
+    if r['desc']: doc.append(str(r['desc']))
+    if r['comment']: doc.append(str(r['comment']))
+    docs  = '\n\n'.join(doc)
 ## rdfs:label
-## dc:description
-## multiline comments ==> """ instead of #:
 
     if lastcls != cls:
       print '\n# %s resources:' % cls
@@ -66,4 +69,4 @@ if __name__ == '__main__':
     if subj.startswith('bsml:'):
       res = subj[5:]
       print ''.join(['  ', '%-14s = ' % res, 'Resource(NS.%-15s' % (res + ')'),
-                     "  #: %s" % doc if doc else ''])
+                     "\n    '''%s'''" % docs if docs else ''])
