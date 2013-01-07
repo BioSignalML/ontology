70acd5037121b50b03a4889ab5f6daca71e436e3
Dave Brooks <dave@bcs.co.nz>
2013-01-07 14:57:07 +1300

    Add a utility script to install an updated BioSignalML ontology.

---
+++ installdoc.sh
@@ -0,0 +1,14 @@
+cp 2011-04-biosignalml.ttl website/biosignalml-$1.ttl
+cp biosignalml.rdf website/biosignalml-$1.rdf
+cp biosignalml.html website/biosignalml-$1.html
+
+cd website
+rm biosignalml.ttl
+rm biosignalml.rdf
+rm biosignalml.html
+ln -s biosignalml-$1.ttl  biosignalml.ttl
+ln -s biosignalml-$1.rdf  biosignalml.ttl
+ln -s biosignalml-$1.html biosignalml.html
+git add biosignalml-$1.ttl  biosignalml.ttl
+git add biosignalml-$1.rdf  biosignalml.rdf
+git add biosignalml-$1.html biosignalml.html
